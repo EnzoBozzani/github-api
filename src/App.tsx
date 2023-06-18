7ce6b95fabcb76f681c539f37355574b658adafe
Enzo Bozzani <bozzanienzo@gmail.com>
2023-06-18 03:36:56 -0300

    feat: developed header and api consumption

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,20 @@
-import User from './ts/User.ts';
+import { useState } from 'react';
 import Header from './components/Header/Header.tsx';
 
-const usernameInput: HTMLInputElement | null = document.querySelector('#username-input');
-
-async function fetchData(username: string){
-    try {
-        const response = await fetch(`https://api.github.com/users/${username}`);        
-        const resJson = await response.json();
-        const user = new User(resJson); 
-    } catch (error){
-        console.log(error);
-    }
-}
-
-document.querySelector('#search-button')?.addEventListener('click', () => {
-    if (usernameInput === null){
-        alert('infomre um nome de usuário valido');
-    } else {
-        fetchData(usernameInput.value);
-    }
-})
-
 export default function App(){
+    const [user, setUser] = useState('');
+    async function fetchUser(username: string) {
+        try {
+            const response = await fetch(`https://api.github.com/users/${username}`).then(res => res.json());
+            setUser(response);
+            console.log(response);
+        } catch (error: any){
+            console.error(error);
+        }
+    }
     return (
         <>
-            <Header/>
+            <Header fetchUser={fetchUser}/>
         </>
     )
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,20 @@
+import { useState } from "react";
 import styles from "./Header.module.scss";
 
-export default function Header(){
+export default function Header(props: {fetchUser: Function}) {
+    const [username, setUsername] = useState("");
     return (
         <header className={styles.header}>
-            <img src="/github.webp" alt="Git Hub Logo" className={styles.githubLogo} />
+            <a href=""><img src="/github.svg" alt="Git Hub Logo" className={styles.githubLogo} /></a>
+            <div className={styles.form}>
+                <div className="input-group mb-3">
+                    <span className="input-group-text" id="basic-addon1">@</span>
+                    <input value={username} onChange={ev => setUsername(ev.target.value)} type="text" className="form-control" placeholder="GitHub Username" aria-label="Username" aria-describedby="basic-addon1" />
+                </div>
+                <button onClick={() => props.fetchUser(username)}>
+                    Search
+                </button>
+            </div>
         </header>
     )
 }
