3769d2938b7515533e8db898770569c70a4b16cd
Enzo Bozzani <bozzanienzo@gmail.com>
2023-07-05 01:49:25 -0300

    feat: made page responsive

--- src/components/FollowSection/User/index.tsx
+++ src/components/FollowSection/User/index.tsx
@@ -5,7 +5,7 @@ const UserCard = (props: {user: FollowerResponse, fetchUser: Function}) => {
     return (
         <div key={props.user.id} className={styles.wrapper} onClick={() => props.fetchUser(props.user.login)}>
             <img src={props.user.avatar_url} alt="" />
-            <p className="display-6 white-color">{props.user.login}</p>
+            <p className="display-6 white-color">{props.user.login.length > 14 ? props.user.login.slice(0, 15) : props.user.login}</p>
             <p className={styles.tip}>Click to search this profile!</p>
         </div>
     )
